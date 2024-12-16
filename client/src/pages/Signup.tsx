81fa49ce168bfa24320441a37edb46f8bb3d88d1
Majexs <166327239+Majexs@users.noreply.github.com>
2024-12-15 21:27:21 -0500

    built out the signup page

---
+++ client/src/pages/Signup.tsx
@@ -0,0 +1,105 @@
+import { useState, type FormEvent, type ChangeEvent } from 'react';
+import { Link } from 'react-router-dom';
+
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+
+import Auth from '../utils/auth';
+
+const Signup = () => {
+    const [formState, setFormState] = useState({
+        name: '',
+        password: '',
+        role: '',
+        username: '',
+    });
+
+    const [addUser, {error, data }] = useMutation(ADD_USER);
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async (event: FormEvent) => {
+        event.preventDefault();
+
+        try {
+            const { data } = await addUser({
+                variables: { input: { ...formState }},
+            });
+
+            Auth.login(data.addUser.token);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    return(
+        <main>
+            <div>
+                <div>
+                    <h4>
+                        <div>
+                            {data? (
+                                <p>
+                                    Success! You may now head{' '}
+                                    <Link to='/'>back to the homepage.</Link>
+                                </p>
+                            ) : (
+                                <form onSubmit={handleFormSubmit}>
+                                    <input
+                                        placeholder="Your name"
+                                        name="name"
+                                        type="text"
+                                        value={formState.name}
+                                        onChange={handleChange}
+                                    />
+                                    <input
+                                        placeholder="********"
+                                        name="password"
+                                        type="text"
+                                        value={formState.password}
+                                        onChange={handleChange}
+                                    />
+                                    <input
+                                        placeholder="Your role"
+                                        name="role"
+                                        type="text"
+                                        value={formState.role}
+                                        onChange={handleChange}
+                                    />
+                                    <input
+                                        placeholder="Your username"
+                                        name="username"
+                                        type="text"
+                                        value={formState.username}
+                                        onChange={handleChange}
+                                    />
+                                    <button
+                                        style={{ cursor: 'pointer'}}
+                                        type="submit"
+                                    >
+                                        Submit
+                                    </button>                                    
+                                </form>
+                            )}
+
+                            {error && (
+                                <div>
+                                    {error.message}
+                                </div>
+                            )}
+                        </div>
+                    </h4>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default Signup;
