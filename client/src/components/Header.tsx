d007bf0d521e9726578e5abc13b0b8cbf884d4d4
Majexs <166327239+Majexs@users.noreply.github.com>
2024-12-15 22:17:08 -0500

    built out header and footer components

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,45 @@
+import { Link } from 'react-router-dom';
+import { type MouseEvent } from 'react';
+import Auth from '../utils/auth';
+
+const Header = () => {
+    const logout = (event: MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        Auth.logout();
+    };
+    return (
+        <header>
+            <div>
+                <div>
+                    <Link to='/'>
+                        <h1>LaxStats</h1>
+                    </Link>
+                    <p>Track your teams stats here! Or something idk...</p>
+                </div>
+                <div>
+                    {Auth.loggedIn()? (
+                        <>
+                            <Link to='/me'>
+                                {Auth.getProfile().data.username}'s profile
+                            </Link>
+                            <button onClick={logout}>
+                                Logout
+                            </button>
+                        </>
+                    ):(
+                        <>
+                            <Link to='/login'>
+                                Login
+                            </Link>
+                            <Link to='signup'>
+                                Signup
+                            </Link>
+                        </>
+                    )}
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
