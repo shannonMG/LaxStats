bd8a46c7d767636ab0f5c357b670f3da5097f718
Majexs <166327239+Majexs@users.noreply.github.com>
2024-12-18 19:44:46 -0500

    kept cleaning up code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,13 +2,10 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import App from './App.jsx';
-import Home from './pages/Home.js';
 import Signup from './pages/Signup.js';
 import Login from './pages/Login.js';
 import Dashboard from './pages/Dashboard.js';
 import ErrorPage from './pages/Error.js';
-import CoachDashboard from './components/CoachDashboard.js';
-import PlayerDashboard from './components/PlayerDashboard.js';
 import StatButton from './components/StatButton.js';
 
 const router = createBrowserRouter([
