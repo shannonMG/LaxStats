5eddd82b8b749e7b1a486b6cdcfb33f820ec8cf5
shukikat <kathuriashuki@gmail.com>
2025-01-07 22:48:25 -0500

    added some changes to background color on player dashboard

--- client/src/components/PlayerDashboard.tsx
+++ client/src/components/PlayerDashboard.tsx
@@ -15,7 +15,7 @@ const PlayerDashboard = () => {
     }
     return (
 
-        <div className="flex flex-col justify-center items-center h-[100vh]">
+        <div className="flex flex-col justify-center items-center h-[100vh] bg-gradient-to-r from-amber-100 to-fuchsia-400">
         <div className="relative flex flex-col items-center rounded-[20px] w-[700px] max-w-[95%] mx-auto bg-white bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:!shadow-none p-3">
             <div className="mt-2 mb-8 w-full">
                 <h4 className="px-2 text-xl font-bold text-navy-700 dark:text-white">
